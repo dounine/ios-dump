b8816c5e22c54dfaa092f0ad09bb317ef02a867d
lake <102535481@qq.com>
2023-01-14 11:43:45 +0800

    增加显示相似度，去掉本地version版本输入

--- index.js
+++ index.js
@@ -38,6 +38,47 @@ function replaceAll(str, match, replacement) {
     return str.replace(new RegExp(escapeRegExp(match), 'g'), () => replacement);
 }
 
+function similar(s, t, f) {
+    if (!s || !t) {
+        return 0
+    }
+    if (s === t) {
+        return 100;
+    }
+    let l = s.length > t.length ? s.length : t.length
+    let n = s.length
+    let m = t.length
+    let d = []
+    f = f || 2
+    let min = function (a, b, c) {
+        return a < b ? (a < c ? a : c) : (b < c ? b : c)
+    }
+    let i, j, si, tj, cost
+    if (n === 0) return m
+    if (m === 0) return n
+    for (i = 0; i <= n; i++) {
+        d[i] = []
+        d[i][0] = i
+    }
+    for (j = 0; j <= m; j++) {
+        d[0][j] = j
+    }
+    for (i = 1; i <= n; i++) {
+        si = s.charAt(i - 1)
+        for (j = 1; j <= m; j++) {
+            tj = t.charAt(j - 1)
+            if (si === tj) {
+                cost = 0
+            } else {
+                cost = 1
+            }
+            d[i][j] = min(d[i - 1][j] + 1, d[i][j - 1] + 1, d[i - 1][j - 1] + cost)
+        }
+    }
+    let res = (1 - d[n][m] / l) * 100
+    return res.toFixed(f)
+}
+
 function calculateHash(filePath) {
     return new Promise((resolve, reject) => {
         let hash = crypto.createHash('sha1')
@@ -57,6 +98,7 @@ function calculateHash(filePath) {
 }
 
 async function main() {
+    console.log('help：退出请输入q或者exit。')
     let dumps = await new Promise((resolve, reject) => {
         request('https://api.ipadump.com/dump/dumps', {
             method: 'GET',
@@ -67,14 +109,41 @@ async function main() {
     dumps.forEach((dump, i) => {
         console.log(`${i}：${dump.name}/${dump.version}`)
     })
+    if (dumps.length === 0) {
+        console.log('当前没有正在申请提取的应用');
+        rl.close();
+        return;
+    }
     let setup = 0;
     let dump = null;
     let mergeName = "";
     let appid = "";
     let version = "";
     let latestDumpIpa = null;
-    let latestVersion = 1;
-    console.log('退出请输入q或者exit')
+
+    const ipas = (await fse.readdir(ipaDir))
+
+    const convertIpas = ipas.map(fileName => {
+        let ipaFile = fs.statSync(path.resolve(ipaDir, fileName))
+        return {
+            fileName,
+            time: ipaFile.mtimeMs
+        };
+    }).filter(item => {
+        return !item.fileName.startsWith('ipadump.com');
+    });
+    if (convertIpas.length === 0) {
+        console.log('不存在未上传文件')
+        return
+    }
+    convertIpas.sort((b, a) => {
+        return a.time - b.time
+    });
+
+    latestDumpIpa = convertIpas[0]
+    const appName = latestDumpIpa.fileName.split('_')[0]
+    mergeName = mergeName || appName || dump.name
+    console.log('未上传最新文件信息：', latestDumpIpa)
     console.log('请输入要提取应用的编号(默认第一个)：')
     rl.on('line', async (line) => {
         if (line.trim() === 'q' || line.trim() === 'exit') {
@@ -84,55 +153,32 @@ async function main() {
         if (setup === 0) {//编号输入
             if (!line.trim()) {
                 dump = dumps[0]
-                console.log('请输入简化名称(默认相同)：')
+                console.log('相似度：' + similar(appName, dump.name))
+                console.log(`请输入简化名称(默认：${mergeName})：`)
                 setup = 1
             } else if (parseInt(line) < dumps.length) {
                 dump = dumps[parseInt(line)]
-                console.log('请输入简化名称(默认相同)：')
+                console.log('相似度：' + similar(appName, dump.name))
+                console.log(`请输入简化名称(默认：${mergeName})：`)
                 setup = 1
             } else {
                 console.log('没有此编号,请检查')
+                rl.close();
+                return;
             }
         } else if (setup === 1) {//简化名称输入
             mergeName = line.trim()
-            console.log('是否是最新版本(默认是)：')
+            console.log(dump)
+            console.log(`处理的信息 -> ${mergeName}:${version} 最新:${dump.latest === 1} 是否开始处理(默认开始):`)
             setup = 2
-        } else if (setup === 2) {//是否是最新版本
             appid = dump.appid;
-            latestVersion = parseInt(line.trim() || '1')
 
-            const ipas = (await fse.readdir(ipaDir))
-
-            const convertIpas = ipas.map(fileName => {
-                let ipaFile = fs.statSync(path.resolve(ipaDir, fileName))
-                return {
-                    fileName,
-                    time: ipaFile.mtimeMs
-                };
-            }).filter(item => {
-                return !item.fileName.startsWith('ipadump.com');
-            });
-            if (convertIpas.length === 0) {
-                console.log('不存在未上传文件')
-                return
-            }
-            convertIpas.sort((b, a) => {
-                return a.time - b.time
-            });
-
-            latestDumpIpa = convertIpas[0]
-            const appName = latestDumpIpa.fileName.split('_')[0]
-            mergeName = mergeName || appName || dump.name
-            if(dump.latest===1){
+            if (dump.latest === 1) {
                 version = dump.version
-            }else{
+            } else {
                 version = latestDumpIpa.fileName.split('_')[1].replace('.ipa', '')
             }
-
-            console.log(dump)
-            console.log(`处理的信息 -> ${mergeName}:${version} 最新:${dump.latest===1} 是否开始处理(默认开始):`)
-            setup = 3
-        } else if (setup === 3) {
+        } else if (setup === 2) {
             console.log(`正在修改 ${appid} 砸壳状态为砸壳中`)
             await new Promise((resolve, reject) => {
                 request('https://api.ipadump.com/dump/update', {
@@ -146,7 +192,7 @@ async function main() {
                         icon: dump.icon,
                         version,
                         des: '官方版本',
-                        latest: dump.latest===1,
+                        latest: dump.latest,
                         status: 1
                     }
                 }, (err, res, body) => {
@@ -183,7 +229,7 @@ async function main() {
                         name: mergeName,
                         lname: dump.name,
                         icon: dump.icon,
-                        latest: dump.latest===1,
+                        latest: dump.latest,
                         status: 2
                     }
                 }, (err, res, body) => {
