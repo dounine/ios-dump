7c3caf67d686e135debc64fbeea821f880b9ed1e
lake <102535481@qq.com>
2023-01-16 10:54:46 +0800

    test

--- test.js
+++ test.js
@@ -2,18 +2,39 @@ import inquirer from 'inquirer';
 
 inquirer
     .prompt([{
-        type: 'confirm',
+        type: 'list',
         name: 'choice',
-        message: '是否确认处理:',
-        default: false,
+        message: '请选择要处理申请：',
+        default: 0,
+        choices: [
+            {value: 1, name: '游戏1'},
+            {value: 2, name: '游戏2'}
+        ]
     }])
     .then(answers => {
         console.log('answers', answers)
     })
     .catch(error => {
-        if(error.isTtyError) {
+        if (error.isTtyError) {
             // Prompt couldn't be rendered in the current environment
         } else {
             // Something else went wrong
         }
     });
+// inquirer
+//     .prompt([{
+//         type: 'confirm',
+//         name: 'choice',
+//         message: '是否确认处理:',
+//         default: true,
+//     }])
+//     .then(answers => {
+//         console.log('answers', answers)
+//     })
+//     .catch(error => {
+//         if (error.isTtyError) {
+//             // Prompt couldn't be rendered in the current environment
+//         } else {
+//             // Something else went wrong
+//         }
+//     });
